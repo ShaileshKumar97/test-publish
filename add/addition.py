32188fad3d2218a099c64bad0b30ec23878d92b9
shaileshkumar97 <kumar.shailesh1597@gmail.com>
2022-11-03 16:50:59 +0530

    docs: Updated the doc-strings

--- add/addition.py
+++ add/addition.py
@@ -1,9 +1,12 @@
-def calculate(a, b):
+def addition(a, b):
     """
     This function adds the two values.
 
     Args:
         a: an integer no
         b: an integer no
+    
+    Returns:
+        added value
     """
     return a + b
