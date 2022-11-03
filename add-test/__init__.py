c2beebbd22331a6a81f29ea6866f27362bb3f077
shaileshkumar97 <kumar.shailesh1597@gmail.com>
2022-11-03 20:21:29 +0530

    chore: Updated package name

---
+++ add-test/__init__.py
@@ -0,0 +1,5 @@
+from add_test.addition import addition
+from add_test.multiply import multiply
+from add_test.subtraction import subtraction
+
+__all__ = ["addition", "multiply", "subtraction"]
--- add/__init__.py
+++
@@ -1,5 +0,0 @@
-from add.addition import addition
-from add.multiply import multiply
-from add.subtraction import subtraction
-
-__all__ = ["addition", "multiply", "subtraction"]
