b081bcb3767f0229b44235a37e4ef43d814dd824
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2022-11-22 19:55:31 +0530

    chore: Added division feature

--- add_testing/__init__.py
+++ add_testing/__init__.py
@@ -1,5 +1,6 @@
 from add_testing.addition import addition
 from add_testing.multiply import multiply
+from add_testing.division import division
 from add_testing.subtraction import subtraction
 
-__all__ = ["addition", "multiply", "subtraction"]
+__all__ = ["addition", "multiply", "division", "subtraction"]
