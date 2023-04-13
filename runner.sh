14fdad7bc473f8e39ec9f81efe3eca6b19acf49f
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2023-04-13 14:02:49 +0530

    ci: Update changelog.md

--- runner.sh
+++ runner.sh
@@ -2,7 +2,6 @@
 # PREV_TAG="$(git tag --sort=-creatordate | sed -n '2 p')"
 PREV_TAG="v1.2"
 echo "Hello world!"
-git pull
 echo "Bug Fixes:" > changelog.md
 git log ${PREV_TAG}..master --grep="^fix" --pretty=format:"%C(auto)- %s %h" >> changelog.md
 echo "" >> changelog.md
