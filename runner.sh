a68cb495bcd23f2e5ee71d4740cfc21f469b8a53
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2023-04-13 13:57:47 +0530

    ci: Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,10 +4,10 @@ PREV_TAG="v1.2"
 echo "Hello world!"
 git pull
 echo "Bug Fixes:" > changelog.md
-git log ${PREV_TAG}..HEAD --grep="^fix" --pretty=format:"%C(auto)- %s %h" >> changelog.md
+git log ${PREV_TAG}..master --grep="^fix" --pretty=format:"%C(auto)- %s %h" >> changelog.md
 echo "" >> changelog.md
 echo "Features:" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s %h" >> changelog.md
+git log ${PREV_TAG}..master --grep="^chore" --pretty=format:"%C(auto)- %s %h" >> changelog.md
 echo "" >> changelog.md
 echo "Merged Pull Requests:" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s %h" >> changelog.md
+git log ${PREV_TAG}..master --grep="^Merge pull request" --pretty=format:"%C(auto)- %s %h" >> changelog.md
