20d7bbaf371af6918dc63b88b5a9fbe47ef86ca7
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2023-04-14 13:10:45 +0530

    ci: Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,10 +2,10 @@
 PREV_TAG="$(git tag --sort=-creatordate | sed -n '2 p')"
 echo "### Bug Fixes" > changelog.md
 echo $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
-git log ${PREV_TAG}..HEAD --grep="^fix" --pretty=format:"%C(auto)- %s ([%h](${{ env.GITHUB_SERVER_URL }}/${{ env.GITHUB_REPOSITORY }}/commit/%H))" | grep fix >> changelog.md
+git log ${PREV_TAG}..HEAD --grep="^fix" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep fix >> changelog.md
 echo "\\" >> changelog.md
 echo "### Features" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s ([%h](${{ env.GITHUB_SERVER_URL }}/${{ env.GITHUB_REPOSITORY }}/commit/%H))" | grep chore >> changelog.md
+git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep chore >> changelog.md
 echo "\\" >> changelog.md
 echo "### Merged Pull Requests" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s ([%h](${{ env.GITHUB_SERVER_URL }}/${{ env.GITHUB_REPOSITORY }}/commit/%H))" | grep "Merge pull request" >> changelog.md
+git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep "Merge pull request" >> changelog.md
