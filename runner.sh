b6bd0bd546b7798bfc69617a9f01197ec9a7315d
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2023-04-16 21:48:42 +0530

    ci: Update changelog.yml

--- runner.sh
+++ runner.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 PREV_TAG="$(git tag --sort=-creatordate | sed -n '2 p')"
-echo "### Bug Fixes" > CHANGELOG.md
+
+# Generate changelogs
+# ($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/compare/v0.14.1...v0.15.0)
+echo $(date +%F) > temp_changelog.md
+echo "---" >> temp_changelog.md
+echo "### Bug Fixes" >> temp_changelog.md
 echo $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
-git log ${PREV_TAG}..HEAD --grep="^fix" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep fix >> CHANGELOG.md
-echo "" >> CHANGELOG.md
-echo "### Features" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep chore >> CHANGELOG.md
-echo "" >> CHANGELOG.md
-echo "### Merged Pull Requests" >> changelog.md
-git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep "Merge pull request" >> CHANGELOG.md
+git log ${PREV_TAG}..HEAD --grep="^fix" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep fix >> temp_changelog.md
+echo "" >> temp_changelog.md
+echo "### Features" >> temp_changelog.md
+git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep chore >> temp_changelog.md
+echo "" >> temp_changelog.md
+echo "### Merged Pull Requests" >> temp_changelog.md
+git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep "Merge pull request" >> temp_changelog.md
+
+# Add generated changelogs to existing changelog file at the top
+if [ -f "CHANGELOG.md" ]; then
+  cat CHANGELOG.md >> temp_changelog.md
+fi
+
+mv temp_changelog.md CHANGELOG.md
