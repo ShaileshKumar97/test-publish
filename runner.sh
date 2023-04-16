8e18bace942246e21dda82dbb731d384b81f8a9f
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2023-04-16 22:21:59 +0530

    ci: Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@ CURRENT_TAG="$(git tag --sort=-creatordate | sed -n '1 p')"
 
 # Generate changelogs
 
-echo "[${CURRENT_TAG#v}]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/compare/${PREV_TAG}...${CURRENT_TAG})" $(date +%F) > temp_changelog.md
+echo "[${CURRENT_TAG#v}]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/compare/${PREV_TAG}...${CURRENT_TAG})" ($(date +%F)) > temp_changelog.md
 echo "---" >> temp_changelog.md
 echo "### Bug Fixes" >> temp_changelog.md
 echo $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
@@ -15,6 +15,7 @@ git log ${PREV_TAG}..HEAD --grep="^chore" --pretty=format:"%C(auto)- %s ([%h]($G
 echo "" >> temp_changelog.md
 echo "### Merged Pull Requests" >> temp_changelog.md
 git log ${PREV_TAG}..HEAD --grep="^Merge pull request" --pretty=format:"%C(auto)- %s ([%h]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/%H))" | grep "Merge pull request" >> temp_changelog.md
+echo >> temp_changelog.md
 
 # Add generated changelogs to existing changelog file at the top
 if [ -f "CHANGELOG.md" ]; then
